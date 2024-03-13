2e1ba5628957af63bf356ce4992d4b5b38779cb3
yusarklnc <erkilincyusaa@gmail.com>
2024-03-13 14:52:10 -0400

    031304

--- src/test/java/pages/Vice_Dean_Management_Page.java
+++ src/test/java/pages/Vice_Dean_Management_Page.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
@@ -8,6 +10,31 @@ public class Vice_Dean_Management_Page {
     public Vice_Dean_Management_Page(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
+
+    //for Add Lesson Program
+    @FindBy(id= "controlled-tab-example-tab-lessonProgram")
+    public WebElement clickonLessonProgram;
+
+    @FindBy (id= "react-select-3-input")
+    public WebElement chooseLesson;
+
+    @FindBy(id = "educationTermId")
+    public WebElement chooseEducationTerm;
+
+    @FindBy(id = "day")
+    public WebElement chooseDay;
+
+    @FindBy(id= "startTime")
+    public WebElement starTime;
+
+    @FindBy(id = "stopTime")
+    public WebElement stopTime;
+
+    @FindBy(xpath = "//*[@id=\"controlled-tab-example-tabpane-lessonProgram\"]/div[2]/div[1]/div/form/div/div[2]/div[3]/div/button")
+    public WebElement submitButtonAddLessonProgram;
+
+    //for Lesson Program List
+
 
 
 
