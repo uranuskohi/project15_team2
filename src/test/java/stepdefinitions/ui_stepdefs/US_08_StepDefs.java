9abdcd0aedffc7adc8a37ae382ce8c5c403ded58
Uranus Kohi <uranuskohi2020@gmail.com>
2024-03-15 16:05:32 +0100

    hi

--- src/test/java/stepdefinitions/ui_stepdefs/US_08_StepDefs.java
+++ src/test/java/stepdefinitions/ui_stepdefs/US_08_StepDefs.java
@@ -1,17 +1,21 @@
 package stepdefinitions.ui_stepdefs;
 
 import io.cucumber.java.en.*;
+import pages.Choose_Lesson_Management_Page;
+import utilities.BrowserUtils;
 
 public class US_08_StepDefs {
 
+    Choose_Lesson_Management_Page chooseLessonManagementPage;
+
     @When("click on Lessons")
     public void click_on_lessons() {
-
+        chooseLessonManagementPage.lessons.click();
     }
 
     @When("enter {string} in the Lesson Name")
     public void enter_in_the_lesson_name(String lessonName) {
-
+        BrowserUtils.sendKeysWithTimeout(chooseLessonManagementPage.lessonNameField, lessonName,2);
     }
 
     @When("check if Compulsory radio button is clickable")
@@ -21,17 +25,17 @@ public class US_08_StepDefs {
 
     @When("click on Compulsory radio button")
     public void click_on_compulsory_radio_button() {
-
+        chooseLessonManagementPage.compulsoryRadioButton.click();
     }
 
     @When("add {string} in the Credit Score")
     public void add_in_the_credit_score(String CreditScore) {
-
+        BrowserUtils.sendKeysWithTimeout(chooseLessonManagementPage.creditScoreField, CreditScore, 2);
     }
 
     @When("click on Submit button")
     public void click_on_submit_button() {
-
+        BrowserUtils.clickWithTimeOut(chooseLessonManagementPage.submitButtonForLessons,2);
     }
 
     @Then("verify the Lesson is created")
