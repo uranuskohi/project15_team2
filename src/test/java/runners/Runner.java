0e280591f274f63540548fbff4047dcc8d10f166
Sloane <sloanealltree@gmail.com>
2024-03-22 14:11:45 -0400

    211

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -7,21 +7,21 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
-//        Don't use it now
-//        plugin = {
-//                "pretty",
-//                "html:target/cucumber-reports.html",
-////                "json:target/json-reports/cucumber.json",
-////                "junit:target/xml-reports/cucumber.xml",
-////                "rerun:target/failedRerun.txt"
-//        },
+
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports.html",
+               "json:target/json-reports/cucumber.json",
+                "junit:target/xml-reports/cucumber.xml",
+                "rerun:target/failedRerun.txt"
+        },
         monochrome = true,
 
 
         features = "./src/test/resources/features",
         glue = {"stepdefinitions", "hooks"},
         dryRun = false,
-        tags = "@US_02"
+        tags = "smoke"
 )
 
 public class Runner {
--- src/test/java/stepdefinitions/ui_stepdefs/US_04_StepDefs.java
+++ src/test/java/stepdefinitions/ui_stepdefs/US_04_StepDefs.java
@@ -97,8 +97,5 @@ public class US_04_StepDefs {
         deanManagementPage.missingDigitNotice.isDisplayed();
     }
 
-    @Given("navigate to {string}")
-    public void navigateTo(String url) {
-        Driver.getDriver().navigate().to("https://managementonschools.com/");
-    }
+
 }
